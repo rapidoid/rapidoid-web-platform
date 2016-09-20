0ea06b34caf1d937dc8860076d9d5873b8822e23
nmihajlovski <nikolce.mihajlovski@gmail.com>
2016-09-20 14:49:37 -0700

    Renamed "tests" to "docker-tests".

--- cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-echo "Clean-up"
-
-docker kill $(docker ps -q) > /dev/null 2>&1 || true
-docker rm $(docker ps -a -q) > /dev/null 2>&1  || true
