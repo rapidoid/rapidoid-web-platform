e88a3d28eae401b8b93f5c19244fe740d1c0da4f
Nikolche Mihajlovski <nikolce.mihajlovski@gmail.com>
2017-12-08 13:23:06 +0100

    Using Rapidoid framework v5.5.0.

--- rapidoid-platform/src/test/java/org/rapidoid/platform/PlatformTestCommons.java
+++ rapidoid-platform/src/test/java/org/rapidoid/platform/PlatformTestCommons.java
@@ -1,27 +1,28 @@
 package org.rapidoid.platform;
 
-/*
- * #%L
- * rapidoid-platform
- * %%
- * Copyright (C) 2014 - 2017 Nikolche Mihajlovski
- * %%
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public
- * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/gpl-3.0.html>.
- * #L%
- */
+	/*
+	 * #%L
+	 * rapidoid-platform
+	 * %%
+	 * Copyright (C) 2014 - 2017 Nikolche Mihajlovski
+	 * %%
+	 * This program is free software: you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as
+	 * published by the Free Software Foundation, either version 3 of the
+	 * License, or (at your option) any later version.
+	 *
+	 * This program is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * You should have received a copy of the GNU General Public
+	 * License along with this program.  If not, see
+	 * <http://www.gnu.org/licenses/gpl-3.0.html>.
+	 * #L%
+	 */
 
+import org.junit.Before;
 import org.rapidoid.annotation.Authors;
 import org.rapidoid.annotation.Since;
 import org.rapidoid.deploy.AppDeployment;
@@ -32,6 +33,11 @@ import java.io.File;
 @Authors("Nikolche Mihajlovski")
 @Since("5.4.6")
 public class PlatformTestCommons extends RapidoidIntegrationTest {
+
+	@Before
+	public final void beforePlatformTest() {
+		PlatformOpts.reset(); // FIXME remove when module registration is fixed
+	}
 
 	static String appPath(String name) {
 		return new File("rapidoid-platform/src/test/" + name).getAbsolutePath();
